a734e1f105f17afa4672be462dda77064bb0c6ec
Alex Fiennes <alex@fiennes.org>
2022-03-18 15:37:31 +0000

    ignore origin check

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -2165,26 +2165,26 @@
             return;
           }
 
-          try {
-            const viewerOrigin = new URL(window.location.href).origin || "null";
-
-            if (HOSTED_VIEWER_ORIGINS.includes(viewerOrigin)) {
-              return;
-            }
-
-            const fileOrigin = new URL(file, window.location.href).origin;
-
-            if (fileOrigin !== viewerOrigin) {
-              throw new Error("file origin does not match viewer's");
-            }
-          } catch (ex) {
-            PDFViewerApplication.l10n.get("loading_error").then(msg => {
-              PDFViewerApplication._documentError(msg, {
-                message: ex?.message
-              });
-            });
-            throw ex;
-          }
+         // try {
+         //   const viewerOrigin = new URL(window.location.href).origin || "null";
+         //
+         //   if (HOSTED_VIEWER_ORIGINS.includes(viewerOrigin)) {
+         //     return;
+         //   }
+         //
+         //   const fileOrigin = new URL(file, window.location.href).origin;
+         //
+         //   if (fileOrigin !== viewerOrigin) {
+         //     throw new Error("file origin does not match viewer's");
+         //   }
+         // } catch (ex) {
+         //   PDFViewerApplication.l10n.get("loading_error").then(msg => {
+         //     PDFViewerApplication._documentError(msg, {
+         //       message: ex?.message
+         //     });
+         //   });
+         //   throw ex;
+         // }
         };
       }
 
